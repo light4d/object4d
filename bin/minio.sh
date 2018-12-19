6ca41266823b992d9f4858676a10b2d9a10f9d04
timeloveboy <734991033@qq.com>
2018-12-19 21:15:36 +0800

    remove redis run

--- bin/minio.sh
+++ bin/minio.sh
@@ -6,7 +6,5 @@ docker run --name object4d-minio -p 9000:9000 -d \
   -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" \
   -e "MINIO_SECRET_KEY=wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY" \
 minio/minio server /data/minio
-docker rm -f object4d-redis
-docker run  --name object4d-redis --net=bridge --restart=always -p 36379:6379 -d redis
 
 docker ps -a|grep object4d
